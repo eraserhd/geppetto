68782b4917dfa32714bf110c13e64145e1ba8ae7
Jason Felice <jason.m.felice@gmail.com>
2024-07-12 13:13:25 -0400

    Whitespace at bol

--- test/net/eraserhead/geppetto/gcode_test.clj
+++ test/net/eraserhead/geppetto/gcode_test.clj
@@ -7,6 +7,7 @@
   (are [line tree] (= tree (gcode/parse-line line))
     "N105"        [[::gcode/line-number 105]]
     "n105"        [[::gcode/line-number 105]]
+    " n105"       [[::gcode/line-number 105]]
     "G-1.4"       [[::gcode/G -1.4]]
     "g-1.4"       [[::gcode/G -1.4]]
     "F2400"       [[::gcode/F 2400.0]]

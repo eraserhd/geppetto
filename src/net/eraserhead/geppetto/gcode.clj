6dc0c1e35e6a0902c1729af580716fa6cbca3c3d
Jason Felice <jason.m.felice@gmail.com>
2024-07-11 20:15:53 -0400

    word-keyword tests

--- src/net/eraserhead/geppetto/gcode.clj
+++ src/net/eraserhead/geppetto/gcode.clj
@@ -1,6 +1,23 @@
 (ns net.eraserhead.geppetto.gcode
-  "Implement of g-code parsing per RS274/NGC Interpreter.")
+  "Implement of g-code parsing per RS274/NGC Interpreter."
+  (:require
+   [clojure.string :as str]))
 
-(defn parse-line [line]
-  "Parse a single line of g-code, presented as a string."
-  {::source line})
+(defn word-keyword [letter]
+  (case letter
+   (";" "(") ::comment
+   (keyword "net.eraserhead.geppetto.gcode" letter)))
+
+(defn parse-words [line]
+  (str/split line #"(?=[ABCDFGHIJKLMNPQRSTXYZ])"))
+
+;(defn parse-line [line]
+;  "Parse a single line of g-code, presented as a string."
+;  (let [words (str/split line #"(?=[ABCDFGHIJKLMNPQRSTXYZ])")]
+;    (transduce (filter (complement empty?))
+;               (fn [block word]
+;                 (let [letter (subs word 0 1)
+;                       value  (read-string (subs word 1))]
+;                   (assoc block (word-keyword letter) value)))
+;               {::source line}
+;               words)))
--- test/net/eraserhead/geppetto/gcode_test.clj
+++ test/net/eraserhead/geppetto/gcode_test.clj
@@ -3,8 +3,21 @@
    [clojure.test :refer [deftest are]]
    [net.eraserhead.geppetto.gcode :as gcode]))
 
-(deftest t-parse-line
-  (are [line k result] (= result (k (gcode/parse-line line)))
-    ""        ::gcode/source ""
-    "; hello" ::gcode/source "; hello"
-    "N100"    ::gcode/source "N100"))
+(deftest t-word-keyword
+  (are [letter kw] (= kw (gcode/word-keyword letter))
+    "G" ::gcode/G
+    "X" ::gcode/X
+    "(" ::gcode/comment
+    ";" ::gcode/comment))
+
+;(deftest t-parse-words [line]
+;  (are [line words] (= words (gcode/parse-words line))
+;    "N100"  [[::gcode/N 100]]))
+
+;(deftest t-parse-line
+;  (are [line k result] (= result (k (gcode/parse-line line)))
+;    ""        ::gcode/source ""
+;    "; hello" ::gcode/source "; hello"
+;    "N100"    ::gcode/source "N100"
+;
+;    "N100"    ::gcode/N      100))

99286af09d647532d2f9abd07c4c6dea4e5fc8c7
Jason Felice <jason.m.felice@gmail.com>
2024-07-12 11:37:34 -0400

    Multiple segments

--- test/net/eraserhead/geppetto/gcode_test.clj
+++ test/net/eraserhead/geppetto/gcode_test.clj
@@ -5,9 +5,10 @@
 
 (deftest t-parse-line
   (are [line tree] (= tree (gcode/parse-line line))
-    "N105"  [[::gcode/line-number 105]]
-    "n105"  [[::gcode/line-number 105]]
-    "G-1.4" [[::gcode/G -1.4]]
-    "g-1.4" [[::gcode/G -1.4]]
-    "F2400" [[::gcode/F 2400]]
-    "f2400" [[::gcode/F 2400]]))
+    "N105"        [[::gcode/line-number 105]]
+    "n105"        [[::gcode/line-number 105]]
+    "G-1.4"       [[::gcode/G -1.4]]
+    "g-1.4"       [[::gcode/G -1.4]]
+    "F2400"       [[::gcode/F 2400]]
+    "f2400"       [[::gcode/F 2400]]
+    "g1x4.2f99z6" [[::gcode/G 1] [::gcode/X 4.2] [::gcode/F 99] [::gcode/Z 6]]))

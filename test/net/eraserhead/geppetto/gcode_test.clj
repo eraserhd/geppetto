195d9c4ecb7c047479da9e9a871b2fac1766fa9d
Jason Felice <jason.m.felice@gmail.com>
2024-07-14 12:39:07 -0400

    Indirect parameter-get

--- test/net/eraserhead/geppetto/gcode_test.clj
+++ test/net/eraserhead/geppetto/gcode_test.clj
@@ -39,6 +39,7 @@
     "f[abs[-6]]"     '[[::gcode/F (abs -6.0)]]
     "f[atan[6]/[4]]" '[[::gcode/F (atan 6.0 4.0)]]
     "f#1"            '[[::gcode/F (parameter-get 1.0)]]
+    "f##1"           '[[::gcode/F (parameter-get (parameter-get 1.0))]]
     "#22=32.4"       '[(parameter-set! 22.0 32.4)]
     "#[1+2]=[3+4]"   '[(parameter-set! (+ 1.0 2.0) (+ 3.0 4.0))]
     "##2=3"          '[(parameter-set! (parameter-get 2.0) 3.0)]))

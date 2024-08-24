a3679695c7e159a4036bc319a627a42d93640f38
Jason Felice <jason.m.felice@gmail.com>
2024-08-24 15:15:34 -0400

    Use m/some

--- src/net/eraserhead/geppetto/gcode.clj
+++ src/net/eraserhead/geppetto/gcode.clj
@@ -92,16 +92,16 @@ decimal                  = [ '+' | '-' ] (( digit {digit} '.' {digit}) | ('.' di
   (->> (re-seq #"(?:([^#%]+)|#\s*(\d+)|#\s*<([^>]*)>|%(d|f|lf)|%\.(\d)f)" text)
        (map (fn [matches]
               (m/match matches
-               [_ (m/pred some? ?text) _ _ _ _]         [::text ?text]
-               [_ _ (m/pred some? ?param-number) _ _ _] [::parameter (Long/parseLong ?param-number)]
-               [_ _ _ (m/pred some? ?param-name) _ _]   [::parameter (normalize-line ?param-name)]
-               [_ _ _ _ "d" _]                          [::format-decimals 0]
-               [_ _ _ _ "f" _]                          [::format-decimals 4]
-               [_ _ _ _ "lf" _]                         [::format-decimals 6]
-               [_ _ _ _ _ (m/pred some? ?digit)]        [::format-decimals (Long/parseLong ?digit)])))))
+               [_ (m/some ?text) _ _ _ _]         [::text ?text]
+               [_ _ (m/some ?param-number) _ _ _] [::parameter (Long/parseLong ?param-number)]
+               [_ _ _ (m/some ?param-name) _ _]   [::parameter (normalize-line ?param-name)]
+               [_ _ _ _ "d" _]                    [::format-decimals 0]
+               [_ _ _ _ "f" _]                    [::format-decimals 4]
+               [_ _ _ _ "lf" _]                   [::format-decimals 6]
+               [_ _ _ _ _ (m/some ?digit)]        [::format-decimals (Long/parseLong ?digit)])))))
 
 (m/defsyntax special-comment [prefix pat]
-  `(m/app (spacey-incasey-prefix-matcher ~prefix) (m/pred some?) ~pat))
+  `(m/app (spacey-incasey-prefix-matcher ~prefix) (m/some ~pat)))
 
 (defn- gcode-comment [& args]
   (m/match (apply str args)

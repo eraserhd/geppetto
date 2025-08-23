013f5bcb0689f18ef70c466361e8b47449e76523
Jason Felice <jason.m.felice@gmail.com>
2025-08-23 14:16:00 -0400

    LT and LE

--- src/net/eraserhead/geppetto/expression/builtins.clj
+++ src/net/eraserhead/geppetto/expression/builtins.clj
@@ -4,7 +4,7 @@
   "Raise a to power b."
   [a b]
   (Math/pow a b))
- 
+
 ;; * MOD
 
 ;; + -
@@ -24,10 +24,13 @@
   [a b]
   (< (abs (- a b)) 1e-6))
 
-(defn ne [a b]
+(defn ne
+  "Not equal."
+  [a b]
   (not (eq [a b])))
 
 (defn gt
+  "Greater than, but not equal to (see eq)."
   {:test #(do
             (assert (gt 2 1))
             (assert (not (gt 0 1e-6))))}
@@ -36,11 +39,29 @@
        (> a b)))
 
 (defn ge
+  "Greater than or equal to (see eq)."
   {:test #(do
             (assert (ge 2 1)))}
   [a b]
   (or (eq a b) (> a b)))
 
-;; GT LT LE
+(defn lt
+  "Less than, but not equal to (see eq)."
+  {:test #(do
+            (assert (lt 1 2))
+            (assert (not (lt 1 1)))
+            (assert (not (lt 0 1e-7))))}
+  [a b]
+  (and (not (eq a b))
+       (< a b)))
+
+(defn le
+  "Less than or equal to (see eq)."
+  {:test #(do
+            (assert (le 1 2))
+            (assert (le 0 1e-7))
+            (assert (le 1e-7 0)))}
+  [a b]
+  (or (eq a b) (< a b)))
 
 ;; AND OR XOR
